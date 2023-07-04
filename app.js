944b1f6c20500ac70db42c3a7b133f3e25f35f4d
Arold Mauger <aroldmauger2022@gmail.com>
2023-07-04 22:45:24 +0200

    Changement dans la route vers le front

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ mongoose.connect(`mongodb+srv://AroldMauger:I_Love_Mongo@cluster0.3vp9fh7.mongod
 
 // Configuration CORS //
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://aroldmauger.github.io/creatures_frontend');
+    res.setHeader('Access-Control-Allow-Origin', 'https://github.com/AroldMauger/creatures_frontend.git');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
