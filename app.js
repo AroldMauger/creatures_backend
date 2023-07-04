c27e7e3edee9a736d036616ca74d5561a6ac1b28
Arold Mauger <aroldmauger2022@gmail.com>
2023-07-04 21:11:18 +0200

    backend

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const path = require('path');
 
 const app = express();
 
+const PORT = process.env.PORT || 4000;
+
 // Connexion à la base de données MongoDB //
 mongoose.connect(`mongodb+srv://${process.env.USER_MONGO}:${process.env.PASSWORD_MONGO}@${process.env.DOMAIN_MONGO}`,
   { useNewUrlParser: true,
@@ -18,28 +20,29 @@ mongoose.connect(`mongodb+srv://${process.env.USER_MONGO}:${process.env.PASSWORD
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // --- Configuration d'Express --- //
-// Toutes les routes appliqueront les règles de CORS
-app.use(cors());
+// Toutes les routes appliqueront les règles de CORS//
+  app.use(cors());  
+// Configuration du middleware pour analyser les requêtes au format JSON //
+  app.use(bodyParser.json());
+  app.use(express.json())
 
-// Configuration du middleware pour analyser les requêtes au format JSON
-app.use(bodyParser.json());
-app.use(express.json());
-
-// Configuration CORS
+// Configuration CORS //
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
-// Routes pour les livres
-app.use("/api/books", bookRoutes);
 
-// Routes pour l'authentification des utilisateurs
-app.use("/api/auth", userRoutes);
+// Routes pour les livres //
+  app.use("/api/books", bookRoutes);
+// Routes pour l'authentification des utilisateurs //
+  app.use("/api/auth", userRoutes);
+// Configuration du middleware pour servir les images statiques du répertoire 'images' //
+  app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// Configuration du middleware pour servir les images statiques du répertoire 'images'
-app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
+
+
